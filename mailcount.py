04c4c27bf08418646a1db64b831a7b2ce3ac1b0e
itsjarvisonly <68685017+itsjarvisonly@users.noreply.github.com>
2024-08-16 11:38:12 +0530

    just a keep up

--- mailcount.py
+++ mailcount.py
@@ -1,2 +1,15 @@
-print("finally its done")
-what is changed
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+dic = {}
+lis = []
+for line in handle:
+    if line.startswith("From "):
+        part = line.split()
+        name = part[1]
+        lis.append(name)
+for k in lis:
+    dic[k] = dic.get(k , 0) + 1
+    print(k)
+    
