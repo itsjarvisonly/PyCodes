b053de57590bb61a696287a688f365690f5674ea
itsjarvisonly <68685017+itsjarvisonly@users.noreply.github.com>
2024-08-23 11:21:04 +0530

    updated calc

--- calculator.py
+++ calculator.py
@@ -13,13 +13,13 @@ n = int(input("How many numbers you want to do work on at a time: "))
 ans = input("Give number 1: ")
 data = [ans]
 for _ in range(n-1):
-  opp = input("what to do (+/-/*or /) :")
+  opp = input("what to do ( + / - / * or /) : ")
   num2 = input("Give next number: ")
   ans = calc(ans,opp,num2)
   data.append((opp,num2))
   print("This step answer is: ",ans)
 print("Your final answer is: ",ans)
-lis = input("Do you want to see list of all the numbers you used y/n ")
-if lis == "y" or lis == "Y"
+lis = input("Do you want to see list of all the numbers you used y/n:  ")
+if lis == "y" or lis == "Y":
   print(data)
 else: print("Done!")
