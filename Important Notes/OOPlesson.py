66d694ec88d4d06ed83843ee6c4b796aed12e676
itsjarvisonly <68685017+itsjarvisonly@users.noreply.github.com>
2024-08-29 10:59:42 +0530

    #4

---
+++ Important Notes/OOPlesson.py
@@ -0,0 +1,24 @@
+#creating class)
+class stud:
+  name = "jarvis"
+  age = 20
+  occ = "hacker"
+  field = "python"
+  team = "blue"
+  house = "shivalik"
+  friend = "pranav"
+  hth = 35
+  wth = 60
+pranav = stud()
+pranav.name = "Pranav"
+pranav.age = 20
+pranav.occ = "student"
+sarla = stud()
+sarla.name = "Sarla"
+sarla.age = 50
+sarla.occ = "housewife"
+
+print("Age of sarla is:" ,sarla.age)
+dat = [1 , 2 , 3 , 4 ,99 ,1000, 5000]
+for x in dat:
+  print("answer is",int(x) * 2)
