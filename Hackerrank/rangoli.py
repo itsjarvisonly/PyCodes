90b425483117ab4c4f9b7deefe4b98bbd5e61dac
Ghanshyam Matlane <68685017+itsjarvisonly@users.noreply.github.com>
2025-09-27 18:39:10 +0000

    change

--- Hackerrank/rangoli.py
+++ Hackerrank/rangoli.py
@@ -1,10 +1,19 @@
 def print_rangoli(size):
     # your code goes here
     lis = list("abcdefghijklmnopqrstuvwxyz")
-    for x in range(size-1):
-      lmax = (size + size -1)*2 - 1 
-      print("-"*lmax + )
+    k = size
+    for x in range(k):
+      l = (k*2-1)*2 - 1 
+      print(int((l-1)/2)*"-", end= "")
+      print(lis[k-1], end="")
+      print(int((l-1)/2)*"-")
+      k -= 1
     
+
+
+
+
+
 
 if __name__ == '__main__':
     n = int(input())
