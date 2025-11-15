15c73bb2a93cac309b10e3d320ec1a09ad7e44f0
Ghanshyam Matlane <68685017+itsghanshyam@users.noreply.github.com>
2025-11-15 16:55:32 +0000

    15-11-2025

--- Hackerrank/alphabet_rangoli.py
+++ Hackerrank/alphabet_rangoli.py
@@ -1,13 +1,20 @@
 def print_rangoli(size):
     # your code goes here
     lis = list("abcdefghijklmnopqrstuvwxyz")
+    l,b = size*2-1,(size*2-1)*2-1
     k = size
-    for x in range(k):
-      l = (k*2-1)*2 - 1 
-      print(int((l-1)/2)*"-", end= "")
-      print(lis[k-1], end="")
-      print(int((l-1)/2)*"-")
-      k -= 1
+    for i in range(size):
+        print(str(lis[k-1]).center(b,"-"))
+        if k > 1:
+            k -= 1
+    for i in range(size-1):
+        print(str(lis[k]).center(b,"-"))
+        if k <= size:
+            k += 1
+        
+        
+        
+    
     
 
 
