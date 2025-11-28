7f76e8b85f883a40655bd583a35b87b9eb376e19
Ghanshyam Matlane <68685017+itsghanshyam@users.noreply.github.com>
2025-11-28 11:36:11 +0000

    solved alphabet rangoli of hackerank

--- Hackerrank/alphabet_rangoli.py
+++ Hackerrank/alphabet_rangoli.py
@@ -1,39 +1,59 @@
 def print_rangoli(size):
     # your code goes here
-    lis = list("abcdefghijklmnopqrstuvwxyz")
-    l,b = size*2-1,(size*2-1)*2-1
+    max = ord("a")+ size - 1
+    l,b = size*2-1,4*size - 3
+    half1 = []
+    rangoli_rows = []
     k = size
     for i in range(size):
-        print(str(lis[k-1]).center(b,"-"))
-        if k > 1:
-            k -= 1
-    for i in range(size-1):
-        print(str(lis[k]).center(b,"-"))
-        if k <= size:
-            k += 1
-        
-        
+        half1.append(chr(max - i))
+        half2 = half1[::-1]
+        half2 = half2[1:]
+        full = half1 + half2
+        midline = "-".join(full)
+        rangoli_rows.append(midline.center(b,'-'))
+        rem_rows = rangoli_rows[::-1]
+        rem_rows = rem_rows[1:]
+    final = rangoli_rows + rem_rows
+    for n in final:
+        print(n)
+
+        # print(midline.center(b,'-'))
         
     
     
-
-
-
+    
+    
+    
+    
+    
+    
+    
+    # l,b = size*2-1,(size*2-1)*2-1
+    # k = size
+    # for i in range(size):
+    #     print(str(lis[k-1]).center(b,"-"))
+    #     if k > 1:
+    #         k -= 1
+    # for i in range(size-1):
+    #     print(str(lis[k]).center(b,"-"))
+    #     if k <= size:
+    #         k += 1
 
 
 
 if __name__ == '__main__':
     n = int(input())
     print_rangoli(n)
-# # needed output
-##  --------e--------
-# # ------e-d-e------
-# # ----e-d-c-d-e----
-# # --e-d-c-b-c-d-e--
-# # e-d-c-b-a-b-c-d-e
-# # --e-d-c-b-c-d-e--
-# # ----e-d-c-d-e----
-# # ------e-d-e------
+# needed output
+#  --------e--------
+# ------e-d-e------
+# ----e-d-c-d-e----
+# --e-d-c-b-c-d-e--
+# e-d-c-b-a-b-c-d-e
+# --e-d-c-b-c-d-e--
+# ----e-d-c-d-e----
+# ------e-d-e------
 # # --------e--------
 
     
