20371679fdf28157a3375637d773f9f6fed0ebb6
Ghanshyam <68685017+itsghanshyam@users.noreply.github.com>
2025-12-29 16:13:32 +0530

    Added new problem 'collections.Counter().py' from hackerrank

---
+++ Hackerrank/collections.Counter().py
@@ -0,0 +1,14 @@
+# A counter is a container that stores elements as dictionary keys, and their counts are stored as dictionary values
+# Question link https://www.hackerrank.com/challenges/collections-counter/problem?isFullScreen=false
+from collections import Counter
+x = int(input())
+shoe = input()
+shoe = (shoe.split(" "))
+shoe = map(int(),shoe)
+print((shoe))
+# for i in shoe:
+#   lis.append(int(i))
+# cust = int(input())
+
+
+
